f34264832de582a1efa72f8d3b2808b618df6393
512kbb <dieg.parraa@duocuc.cl>
2023-10-30 10:13:40 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithValidations.Models;
+
+namespace DojoSurveyWithValidations.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("results")]
+    public IActionResult ResultSurvey(Survey survey)
+    {
+        Console.WriteLine(survey.Name);
+        return View(survey);
+
+    }
+
+    [HttpPost]
+    [Route("process")]
+    public IActionResult ProcessSurvey (Survey survey)
+    {
+        if (ModelState.IsValid)
+        {
+            Console.WriteLine("si");
+            return RedirectToAction("ResultSurvey", survey);
+        }
+
+        Console.WriteLine("Not valid");
+        return View("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyWithValidations.Models
+{
+    public class Survey
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters long!")]
+        public string Name {get; set;}
+        [Required]
+        public string DojoLocation {get; set;}
+        [Required]
+        public string FavoriteLanguage {get; set;}
+        [MinLength(20)]
+        public string? Comment {get; set;}
+
+    }
+}
